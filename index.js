263a49c25c9ae05a1fea0cc67b1a95f820daf7e3
lowercasewords <87104601+lowercasewords@users.noreply.github.com>
2022-05-12 14:02:30 -0700

    test code

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
-function fillBody () {
-    fillCounterParr()
-}
-function fillCounterParr () {
-    const counterSpan = document.getElementById('counter').textContent = `Pressed ${++counter} times`;
-    let counter = 0;
+const nav = window.document.getElementsByTagName("nav")[0];
+let navBarClosed = true;
+swapNavBar();
+
+function swapNavBar() {
+    const navBar = document.createElement('div');
+    if (navBarClosed) { // expand the nav
+        const toDelete = document.getElementById('nav-box-closed');
+        if(toDelete != null) {
+            toDelete.remove();
+        }
+        navBar.id = 'nav-box-opened';
+        
+        // add it to its 'boder'
+        // navBar.setAttribute('ondblclick', 'swapNavBar()');
+        const pseudoBorder = navBar.appendChild(document.createElement('div'));
+        pseudoBorder.id = 'nav-box-opened-pseudo-border';
+        console.log('made opened container');
+    }
+    else { // shrink nav
+        const toDelete = document.getElementById('nav-box-opened');
+        if(toDelete != null) {
+            toDelete.remove();
+        }
+        navBar.id = 'nav-box-closed';
+        console.log('made closed container');
+        navBar.setAttribute('onclick', 'swapNavBar()');
+    }
+    nav.appendChild(navBar);
+    navBarClosed = !navBarClosed;
 }
